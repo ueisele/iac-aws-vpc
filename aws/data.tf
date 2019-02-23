a6f99c1f2301542630313eb42da03be16d02d5da
Uwe Eisele <code@uweeisele.eu>
2019-02-23 20:55:34 +0100

    WIP: AWS Networking - Retrieve current region and use this

--- aws/data.tf
+++ aws/data.tf
@@ -1,4 +1,10 @@
 /*
+ * The region to be used.
+ * https://www.terraform.io/docs/providers/aws/d/region.html
+ */
+data "aws_region" "current" {}
+
+/*
  * Retrieve the AZ where we want to create network resources
  * This must be in the region selected on the AWS provider.
  * https://www.terraform.io/docs/providers/aws/d/availability_zone.html
@@ -6,6 +12,6 @@
 data "aws_availability_zone" "availability_zones" {
     count = "${length(var.availability_zones)}"
     
-    name = "${var.region}${element(var.availability_zones, count.index)}"
+    name = "${data.aws_region.current.name}${element(var.availability_zones, count.index)}"
 }
 
